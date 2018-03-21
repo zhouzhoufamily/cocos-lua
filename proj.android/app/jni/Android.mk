789c9e2e95e29e83e5bfe4fcfff41f86e076e0e3
zhouzhoufamily <512694189@qq.com>
2018-03-21 15:51:43 +0800

    refrence engine.

--- proj.android/app/jni/Android.mk
+++ proj.android/app/jni/Android.mk
@@ -2,10 +2,10 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-$(call import-add-path,$(LOCAL_PATH)/../../../cocos2d)
-$(call import-add-path,$(LOCAL_PATH)/../../../cocos2d/external)
-$(call import-add-path,$(LOCAL_PATH)/../../../cocos2d/cocos)
-$(call import-add-path,$(LOCAL_PATH)/../../../cocos2d/cocos/audio/include)
+$(call import-add-path,$(LOCAL_PATH)/../../../../..)
+$(call import-add-path,$(LOCAL_PATH)/../../../../../external)
+$(call import-add-path,$(LOCAL_PATH)/../../../../../cocos)
+$(call import-add-path,$(LOCAL_PATH)/../../../../../cocos/audio/include)
 
 LOCAL_MODULE := MyGame_shared
 
@@ -28,7 +28,7 @@ LOCAL_STATIC_LIBRARIES := cocos2dx_static
 
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-add-path, $(LOCAL_PATH)/../../../cocos2d)
+$(call import-add-path, $(LOCAL_PATH)/../../../../..)
 $(call import-module, cocos)
 
 # _COCOS_LIB_IMPORT_ANDROID_BEGIN
