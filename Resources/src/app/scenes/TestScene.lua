1f8136f5276f16c60cfaa19a4eface1655b31098
zhouzhoufamily <512694189@qq.com>
2018-04-09 18:54:46 +0800

    update TestScene.

--- Resources/src/app/scenes/TestScene.lua
+++ Resources/src/app/scenes/TestScene.lua
@@ -4,29 +4,36 @@ end)
 
 function TestScene:ctor()
 
+    -- get windows size.
     local winSize = cc.Director:getInstance():getVisibleSize()
 
-    -- add background image
+    -- add background image.
     display.newSprite("HelloWorld.png")
         :move(display.center)
         :addTo(self)
 
-    -- add HelloWorld label
+    -- add HelloWorld label.
     cc.Label:createWithSystemFont("Test Scene", "Arial", 40)
         :move(display.cx, display.cy + 200)
         :addTo(self)
 
-        -- "CloseNormal.png","CloseSelected.png"
-    cc.ControlButton:create()
-        :setTitleForState("Load Data", 1)
-        :move(winSize.width / 2, winSize.height / 2)
-        -- :addTargetWithActionForControlEvents(function(sender)
-        --     cclog("click...")
-        -- end)
-        :addTo(self)
+    -- add close button.
+    local btnN = cc.Scale9Sprite:create("CloseNormal.png")
+    local btnS = cc.Scale9Sprite:create("CloseSelected.png")
+    local controlButton = cc.ControlButton:create(btnN)
+    local menuSize = controlButton:getContentSize()
+    controlButton:setBackgroundSpriteForState(btnS, cc.CONTROL_STATE_HIGH_LIGHTED)
+    controlButton:setPosition(winSize.width - menuSize.width - 5, winSize.height - menuSize.height - 5)
+    self:addChild(controlButton)
+    controlButton:registerControlEventHandler(self.TouchDownAction, cc.CONTROL_EVENTTYPE_TOUCH_DOWN)
 
+    -- play bg music.
         -- ccexp.AudioEngine:play2d("sounds/bg.mp3", true);
 end
+
+function TestScene:TouchDownAction()  
+    cc.Director:getInstance():endToLua()
+end  
 
 function TestScene:onEnter()
 end
